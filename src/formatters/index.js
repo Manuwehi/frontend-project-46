dfc855476b17af447fd83fba5d0530ea85a10dad
Alex <manuwehial@gmail.com>
2024-07-08 00:12:07 +0500

    9 step almost completed

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,12 @@
 import plainFormat from './plain.js';
 import stylishFormat from './stylish.js';
+import jsonFormat from './json.js';
 
 const formatter = (diff, format) => {
   switch (format) {
     case ('stylish'): return stylishFormat(diff);
     case ('plain'): return plainFormat(diff);
+    case ('json'): return jsonFormat(diff);
     default: return `Wrong format ${format}`;
   }
 };
