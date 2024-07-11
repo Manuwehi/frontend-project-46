594905b5d3cbb8e305366bffad1ff34c6b81902b
Alex <manuwehial@gmail.com>
2024-07-11 20:07:01 +0500

    Changed test name

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test.each(['json', 'yaml', 'yml'])('test', (ext) => {
+test.each(['json', 'yaml', 'yml'])('Testing differ & form', (ext) => {
   expect(gendiff(getFixturePath(`file1.${ext}`), getFixturePath(`file2.${ext}`))).toStrictEqual(readFile('expectedStylish.txt'));
   expect(gendiff(getFixturePath(`file1.${ext}`), getFixturePath(`file2.${ext}`), 'stylish')).toStrictEqual(readFile('expectedStylish.txt'));
   expect(gendiff(getFixturePath(`file1.${ext}`), getFixturePath(`file2.${ext}`), 'plain')).toStrictEqual(readFile('expectedPlain.txt'));
